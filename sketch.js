14948952f7179e38c536c3e916b312dc9bbf7388
kaitabuchi <93952418+thecodingcookery@users.noreply.github.com>
2023-05-13 21:35:48 -0500

    Starter code

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+function setup() {
+  createCanvas(400, 400);
+  
+}
+
+function draw() {
+  background(0);
+  noFill();
+  stroke(255, 0, 0);
+  strokeWeight(10);
+  
+  ellipse(200, 200, 200, 200);
+}
+
